3dc7a5d8becc35a2978ad5cf311df01efd824898
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-21 13:32:43 -0400

    add more sort tests

--- test/sort.test.js
+++ test/sort.test.js
@@ -2,8 +2,31 @@ const expect = require('chai').expect;
 const sort = require('../sort');
 
 describe('Sort function', () => {
-  it('should sort an array', () => {
-    expect(sort([10, 9, 8, 7, 6])).to.eql([6, 7, 8, 9, 10]);
+  it('should sort numbers from low to high', () => {
+    expect(sort([1.2, -9.3, 0, 8, 7, 6])).to.eql([-9.3, 0, 1.2, 6, 7, 8]);
   });
 
+  it('should sort strings alphabetically, case-sensitively', () => {
+    expect(sort(['Stuff', 'zebras', 'Ben', 'andrew'])).to.eql([
+      'Ben',
+      'Stuff',
+      'andrew',
+      'zebras'
+    ]);
+  });
+
+  it('should work with duplicates', () => {
+    expect(sort([1.2, 1.2, 1.2, -9.3, -9.3, -9.3, 0, 8, 7, 6])).to.eql([
+      -9.3,
+      -9.3,
+      -9.3,
+      0,
+      1.2,
+      1.2,
+      1.2,
+      6,
+      7,
+      8
+    ]);
+  });
 });
